5a881dcd806be66e41f0f8fcd0e17e907fcd0832
an.nguyen <thienannguyen.cv@gmail.com>
2025-03-30 15:44:29 +0700

    Undo some vtest changes.

--- tests/vtest_smap.py
+++ tests/vtest_smap.py
@@ -155,55 +155,6 @@ class SMap3x3VTestCase(unittest.TestCase):
             print(f"actual: {actual}")
             print(f"expected: {expected}")
             raise e
-    
-    def test_out_r_1st_stage(self):
-        test_types = ["above_left", "above", "above_right", "right", "below_right", "below", "below_left", "left"]
-        test_type = test_types[np.random.choice(range(len(test_types)), size=None)]
-        smap3x3 = self.smap3x3.to(self.device)
-        self.vtestcase = TestCase(name=f"test_{test_type}_target", testbot_in=TestBot_In(offset_in=3), testbot_out=TestBot_Out(), testbot_target=TestBot_Target())
-        
-        z = 1e3*np.random.rand(1)[0]
-        input_repr = (z*(self.input_mask))
-        input_repr = smap3x3.to_3d(input_repr.reshape(1,1, self.input_mask.shape[0], self.input_mask.shape[1]), self.input_mask.shape[0], self.input_mask.shape[1], self.panel, self.input_mask.shape).reshape(3,3,3, self.input_mask.shape[0], self.input_mask.shape[1])
-        input_repr = nn.Parameter(input_repr[1:2,1:2,:,:,:], requires_grad=True).to(self.device)
-        
-        input_mask = self.input_mask.reshape(1,1,1, self.input_mask.shape[0], self.input_mask.shape[1])
-        input_repr = nn.Parameter(torch.cat([input_repr, input_mask], dim=2), requires_grad=True).to(self.device)
-        
-        target_repr = torch.from_numpy(np.load(f"./tests/vtest_data/smap3x3/{test_type}_target.npy"))
-        target_repr = (1.*(target_repr)).reshape(1,1, self.input_mask.shape[0], self.input_mask.shape[1]).to(self.device)
-        target_repr = self.vtestcase.testbot_target(target_repr)
-        
-        
-        smap3x3.zero_grad()
-        input_repr = self.vtestcase.testbot_in(input_repr)
-        weights = smap3x3(input_repr, target_repr, self.input_mask.shape)
-        weights = self.vtestcase.testbot_out(weights)
-        weights_m = torch.max(weights.reshape(1,-1, self.input_mask.shape[0], self.input_mask.shape[1]),dim=-1,keepdim=True).values
-        
-        loss = torch.sum(torch.abs(weights-target_repr), dim=1, keepdim=True)
-        loss_m = torch.abs(weights_m-target_repr)
-        
-        loss = torch.mean(torch.sum(loss_m.detach()+(loss-loss.detach()), dim=-1))
-        loss.backward()
-        
-        with open(f'./tests/vtest_data/output/test_{test_type}_target_flow_info.pkl', 'rb') as file:
-            flow_data = pickle.load(file)
-            print(f'./tests/vtest_data/output/test_{test_type}_target_flow_info.pkl')
-            print((flow_data["activation_gradients"])["in"])
-            actual = ((flow_data["activation_gradients"])["in"] != 0).astype(int)
-        print("actual")
-        print(actual)
-                    
-        expected = np.load(f"./tests/vtest_data/smap3x3/{test_type}_target.npy").astype(int)
-        
-        try:
-            np.testing.assert_array_equal(actual, expected,
-                                       err_msg="Gradient test with 'above' target failed.")
-        except Exception as e:
-            print(f"actual: {actual}")
-            print(f"expected: {expected}")
-            raise e
             
 
 if __name__ == "__main__":
